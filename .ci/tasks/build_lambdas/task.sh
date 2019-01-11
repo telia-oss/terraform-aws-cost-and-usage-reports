cf997e4e14d5c606f6bd389ea72f5922c71dbb2e
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-11 19:00:58 +0100

    add debug line

--- .ci/tasks/build_lambdas/task.sh
+++ .ci/tasks/build_lambdas/task.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-mkdir -p /dist
+mkdir -p source/dist
 source/docker/build.sh
 export DIR="${PWD}"
 cd source
-mkdir -p /dist
 make build-csv-processor
 make build-manifest-processor
 make build-bucket-forwarder
