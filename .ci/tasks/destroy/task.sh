33bcb0e0305534d873003a05c32a8bf0e48488c6
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-12 01:19:41 +0100

    more secret source to go

--- .ci/tasks/destroy/task.sh
+++ .ci/tasks/destroy/task.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export DIR="${PWD}"
-cd ${DIR}/secret-source/examples/${directory}
+cd ${DIR}/source/examples/${directory}
 rm -rf .terraform
 terraform init
 terraform destroy --auto-approve
