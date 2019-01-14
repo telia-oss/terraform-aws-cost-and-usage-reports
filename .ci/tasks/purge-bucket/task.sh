cf71f0b468a1207667a6b19d1f65cd1f4a6dab37
Colin Coleman <colin.coleman@kontrollsmart.com>
2019-01-14 11:43:10 +0100

    remove random text pasted in wrong place

--- .ci/tasks/purge-bucket/task.sh
+++ .ci/tasks/purge-bucket/task.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 export AWS_DEFAULT_REGION=eu-west-1
 BUCKET_NAME=`cat terraform-out/terraform-out.json | jq -r '.lambda_deployment_bucket.value'`
 
-
-$ aws s3 rm s3://$BUCKET_NAME --recursive
+aws s3 rm s3://$BUCKET_NAME --recursive
