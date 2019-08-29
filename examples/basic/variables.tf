ef39d5ecf19bd8c5118136975c233f9d02f08453
itsdalmo <kristian@doingit.no>
2019-08-29 15:58:43 +0200

    Fix uploading of artifacts in basic example

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -3,6 +3,11 @@ variable "name_prefix" {
   default = "cost-and-usage-basic-example"
 }
 
+variable "lambda_artifacts" {
+  type    = list(string)
+  default = ["bucket_forwarder.zip", "csv_processor.zip", "manifest_processor.zip"]
+}
+
 variable "region" {
   type    = string
   default = "eu-west-1"
