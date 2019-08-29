d8dc131f3432eb45aa86f692bec220fa5d6d3558
itsdalmo <kristian@doingit.no>
2019-08-29 09:56:52 +0200

    Add type information to variables

--- modules/report-forwarding/variables.tf
+++ modules/report-forwarding/variables.tf
@@ -1,23 +1,28 @@
 variable "prefix" {
   description = "Prefix used for resource names."
+  type        = string
   default     = "cost-and-usage-report"
 }
 
 variable "source_bucket" {
   description = "Bucket where lambda funtions are uploaded."
+  type        = string
 }
 
 variable "source_path" {
   description = "Bucket path where lambda functions are uploaded."
+  type        = string
   default     = "lambda"
 }
 
 variable "report_bucket" {
   description = "Bucket name where reports will be saved."
+  type        = string
 }
 
 variable "aws_billing_account_id" {
   description = "Magic AWS account number that uploads the billing reports."
+  type        = string
   default     = "386209384616"
 }
 
