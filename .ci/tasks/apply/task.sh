2fbfbf9f2c7fe5d4ecdf877d5011d5c8f56f3410
itsdalmo <kristian@doingit.no>
2019-08-29 13:02:47 +0200

    Remove old test files

--- .ci/tasks/apply/task.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export DIR="${PWD}"
-cd ${DIR}/source/examples/${directory}
-terraform init
-terraform apply --auto-approve
-terraform output -json > ${DIR}/terraform-out/terraform-out.json
